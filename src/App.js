92ffc02049d508ee470ef190aa009ae5ec6574af
Deeva-Civia <s22210557@student.unklab.ac.id>
2024-11-17 13:41:41 +0800

    create a home page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import './assets/css/styles.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div classname="App">
+        <nav>
+          <ul className="nav-links">
+            <li><a href="#home" className="active">Home</a></li>
+            <li><a href="#">Skills</a></li>
+            <li><a href="#">Experience</a></li>
+            <li><a href="#">Certificate</a></li>
+            <li><a href="#">Project</a></li>
+          </ul>
+        </nav>
+        <main id="home" className="content">
+            <div className="text-content">
+              <p className="greeting">Hi, I'm Deeva Civia Aulia Lolong</p>
+              <h1 className="title">
+                <span className="purple">Web Developer &amp;</span>
+                <span className="purple">Design Enthusiast</span>
+              </h1>
+              <p className="description">
+                I'm a 5th-semester university student with a strong interest in web development and a love for creating visually stunning designs. Combining technical skills and creativity, I strive to build user-friendly and aesthetically pleasing web experiences.
+              </p>
+            </div>
+            <div className="image-container">
+              <div className="background-square-2" />
+              <div className="background-square" />
+              <div className="profile-frame">
+                <img src="./img/foto porto.png" alt="Profile" className="profile-image" />
+              </div>
+              <div className="blur-circle pink-circle" />
+              <div className="blur-circle blue-circle" />
+              <div className="decorative-dot dot-1" />
+              <div className="decorative-dot dot-2" />
+              <div className="decorative-dot dot-3" />
+              <div className="decorative-dot dot-4" />
+            </div>
+        </main>
     </div>
   );
 }
