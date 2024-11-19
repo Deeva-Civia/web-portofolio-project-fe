298e3b5aa33bff1f5a0c34417eeaa20daef851df
Deeva-Civia <s22210557@student.unklab.ac.id>
2024-11-19 15:58:26 +0800

    modify Certificate, delete CertificateCard, add integration with firebase

--- src/component/Certificate/CertificateCard/index.js
+++
@@ -1,9 +0,0 @@
-const CertificateCard = ({image, alt}) => {
-    return (
-        <div className="certificate-card">
-            <img src={image} alt={alt} />
-        </div>
-    )
-};
-
-export default CertificateCard;
--- src/component/Certificate/index.js
+++ src/component/Certificate/index.js
@@ -1,32 +1,42 @@
-import CertificateCard from "./CertificateCard";
+import { getDatabase, ref, onValue } from "firebase/database";
+import { useEffect, useState} from "react";
 
 const Certificate = () => {
-	const certificatesData = [
-		{ image: "img/sertifikat alibaba.png", alt: "Certificate from Alibaba" },
-		{ image: "img/sertifikat bootstrap.png", alt: "Certificate Bootstrap" },
-		{ image: "img/sertifikat dicoding.png", alt: "Certificate from Dicoding" },
-		{ image: "img/sertifikat javascript.png", alt: "Certificate Javascript" }
-	];
-	const bottomCertificatesData = [
-		{ image: "img/sertifikat ksn nasional.png", alt: "Certificate KSN Astronomi" },
-		{ image: "img/sertifikat sololearn.png", alt: "Certificate from Sololearn" }
-	];
+	const [certificate, setCertificate] = useState({});
+
+    useEffect(() => {
+        const db = getDatabase();
+        const heroRef = ref(db, "certificate/");
+        onValue(heroRef, (snapshot) => {
+            const data = snapshot.val();
+            setCertificate(data);
+            });
+    }, []);
 	return (
 		<section id="certificate">
-			<h2 className="sub-title">Certificate</h2>
-			<div className="certificates-grid">
-				{certificatesData.map((cert, index) => (
-				<CertificateCard key={index} {...cert} />
-				))}
+				<h2 className="sub-title">{certificate.title}</h2>
+				<div className="certificates-grid">
+				<div className="certificate-card">
+					<img src={`data:image/jpg;base64, ${certificate.image1}`} alt="Certificate 1" />
+				</div>
+				<div className="certificate-card">
+					<img src={`data:image/jpg;base64, ${certificate.image2}`} alt="Certificate 2" />
+				</div>
+				<div className="certificate-card">
+					<img src={`data:image/jpg;base64, ${certificate.image3}`} alt="Certificate 3" />
+				</div>
+				<div className="certificate-card">
+					<img src={`data:image/jpg;base64, ${certificate.image4}`} alt="Certificate 4" />
+				</div>
+				<div className="certificate-card">
+					<img src={`data:image/jpg;base64, ${certificate.image5}`} alt="Certificate 5" />
+				</div>
+				<div className="certificate-card">
+					<img src={`data:image/jpg;base64, ${certificate.image6}`} alt="Certificate 6" />
+				</div>
 			</div>
-			<div className="bottom-certificates">
-				{bottomCertificatesData.map((cert, index) => (
-				<CertificateCard key={index} {...cert} />
-				))}
-			</div>
-			<div className="blur-circle blue-circle" />
 		</section>
-	);
-};
+	)
+}
 
 export default Certificate;
