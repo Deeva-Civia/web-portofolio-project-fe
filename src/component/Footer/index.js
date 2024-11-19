3c7326b9fa02450832ac1628e5218501485568e0
Deeva-Civia <s22210557@student.unklab.ac.id>
2024-11-19 18:26:57 +0800

    modify Footer, add integration with firebase

--- src/component/Footer/index.js
+++ src/component/Footer/index.js
@@ -1,8 +1,21 @@
+import { getDatabase, ref, onValue } from "firebase/database";
+import { useEffect, useState} from "react";
+
 const Footer = () => {
+    const [footer, setFooter] = useState({});
+
+    useEffect(() => {
+        const db = getDatabase();
+        const heroRef = ref(db, "footer/");
+        onValue(heroRef, (snapshot) => {
+            const data = snapshot.val();
+            setFooter(data);
+            });
+    }, []);
     return (
         <footer className="footer-container">
             <div className="footer-content">
-                <p>&copy; Copyright 2024, Portofolio by Deeva Civia Aulia Lolong</p>
+                <p>&copy; {footer.copyRight}</p>
             </div>
         </footer>
     );
